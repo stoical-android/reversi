7558947b0068804541410897f6491bce5dcd34cb
stoical-android <klimstein.mt@gmail.com>
2016-05-01 12:44:36 +0200

    Implement creation of game board

--- Application/mainwindow.cpp
+++ Application/mainwindow.cpp
@@ -6,9 +6,25 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    createGameBoard();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::createGameBoard()
+{
+    for (unsigned row = 0; row < 8; ++row)
+    {
+        for (unsigned col = 0; col < 8; ++col)
+        {
+            QPushButton* field = new QPushButton();
+            field->setFixedSize(45, 45);
+            ui->grid_layout->addWidget(field, row, col);
+        }
+    }
+    ui->grid_layout->setHorizontalSpacing(0);
+    ui->grid_layout->setVerticalSpacing(0);
 }
--- Application/mainwindow.h
+++ Application/mainwindow.h
@@ -17,6 +17,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void createGameBoard();
 };
 
 #endif // MAINWINDOW_H
